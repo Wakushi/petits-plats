626cc97b8f3f077f85d523c1bc56c90eaf46fa88
makushi <mzozord@gmail.com>
2024-06-06 09:30:47 +0200

    Project init

---
+++ src/components/search-bar.ts
@@ -0,0 +1,18 @@
+export default class SearchBar {
+  searchBarElement: HTMLInputElement
+  constructor() {
+    this.searchBarElement = document.querySelector(
+      "#searchBar"
+    ) as HTMLInputElement
+    this.bindSearchBar()
+  }
+
+  bindSearchBar() {
+    this.searchBarElement.addEventListener("keyup", this.handleSearch)
+  }
+
+  handleSearch = (event: KeyboardEvent) => {
+    const searchValue = this.searchBarElement.value
+    console.log(searchValue)
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import "./style.css"
+import SearchBar from "./components/search-bar"
+
+class App {
+  static init() {
+    new SearchBar()
+  }
+}
+
+App.init()
